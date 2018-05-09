a951732b971ffcf02d398f0a99f7a72f8864bbe2
SoumyAmiya <amiya.ranjandash22@gmail.com>
2018-05-09 15:47:02 +0530

    add StaticFiles

--- LoginApp/urls.py
+++ LoginApp/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from django.urls import path
 from LoginApp import views
 urlpatterns=[
-    url(r'^login/',views.index),
+    url(r'^homepage/',views.home),
     url(r'^Register/',views.signup),
-    path('index/', views.home),
-    path('login/signup/', views.signup),
-    path('login/success/', views.success),
-    path('login/loging/', views.loginform),
-    path('login/verify/', views.login)
+    #path('index/', views.home),
+    path('signup/', views.signup),
+    path('success/', views.success),
+    path('existinguser/', views.loginform),
+    path('verify/', views.login)
+
 ]
